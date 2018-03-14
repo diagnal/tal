2d1f6450a90cf61ec1197e019368eefb7fbcc646
Arun Antoney <arun.antoney@diagnal.com>
2018-03-14 19:50:06 +0530

    autherization header added

--- static/script/devices/device.js
+++ static/script/devices/device.js
@@ -875,10 +875,7 @@ define(
                         onLoad: opts.onLoad,
                         onError: opts.onError,
                         headers: {
-                            'Content-Type': 'application/json',
-                            'Access-Control-Allow-Origin': '*',
-                            'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
-                            'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token'
+                            'Content-Type': 'application/json'
                         },
                         data: payload,
                         method: 'POST'
